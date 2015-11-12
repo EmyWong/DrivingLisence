bd4258a0f8b7abbcb0360960b53ec1577ef072bf
张飞 <1015947235@qq.com>
2015-11-12 17:07:14 +0800

    驾友讨论cell

---
+++ DriveTest/DriveTest/Classes/General/Views/驾友讨论Cell/JiaYouTaoLunCell.h
@@ -0,0 +1,13 @@
+//
+//  JiaYouTaoLunCell.h
+//  DriveTest
+//
+//  Created by lanou3g on 15/11/12.
+//  Copyright © 2015年 第一小组. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JiaYouTaoLunCell : UITableViewCell
+
+@end
---
+++ DriveTest/DriveTest/Classes/General/Views/驾友讨论Cell/JiaYouTaoLunCell.m
@@ -0,0 +1,23 @@
+//
+//  JiaYouTaoLunCell.m
+//  DriveTest
+//
+//  Created by lanou3g on 15/11/12.
+//  Copyright © 2015年 第一小组. All rights reserved.
+//
+
+#import "JiaYouTaoLunCell.h"
+
+@implementation JiaYouTaoLunCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
