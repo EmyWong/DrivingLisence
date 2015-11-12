65192370ab36598a07fc665e771de06500b4b5ac
jinke <853834951@qq.com>
2015-11-12 11:35:29 +0800

    验证码倒计时完成

--- DriveTest/DriveTest/Classes/Scenes/ViewController/驾考TVC/RegisterViewController.m
+++ DriveTest/DriveTest/Classes/Scenes/ViewController/驾考TVC/RegisterViewController.m
@@ -40,7 +40,34 @@
     
     [SMSSDK getVerificationCodeByMethod:SMSGetCodeMethodSMS phoneNumber:self.registerPhoneTextField.text zone:@"86" customIdentifier:nil result:^(NSError *error) {
         if (!error) {
-            NSLog(@"获取验证码成功");
+            __block int timeout=59; //倒计时时间
+            dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+            dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
+            dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+            dispatch_source_set_event_handler(_timer, ^{
+                if(timeout<=0){ //倒计时结束，关闭
+                    dispatch_source_cancel(_timer);
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        //设置界面的按钮显示 根据自己需求设置
+                        [_verifyButton setTitle:@"获取验证码" forState:UIControlStateNormal];
+                        _verifyButton.userInteractionEnabled = YES;
+                    });
+                }else{
+                    //            int minutes = timeout / 60;
+                    int seconds = timeout % 60;
+                    NSString *strTime = [NSString stringWithFormat:@"%.2d", seconds];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        //设置界面的按钮显示 根据自己需求设置
+                        NSLog(@"____%@",strTime);
+                        [_verifyButton setTitle:[NSString stringWithFormat:@"剩余%@秒",strTime] forState:UIControlStateNormal];
+                        _verifyButton.userInteractionEnabled = NO;
+                        
+                    });
+                    timeout--;
+                    
+                }
+            });
+            dispatch_resume(_timer);
         }else
         {
             UIAlertController *alertController = nil;
